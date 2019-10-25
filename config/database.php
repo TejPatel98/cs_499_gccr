801d4b515d5ae443508e01a1bc062a1e03357074
Tom Busby <tmbu225@uky.edu>
2019-10-25 15:09:53 -0400

    Added separate OVS database connection.

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'ovs' => [
+            'driver' => 'mysql',
+            'host' => env('OVS_DB_HOST', '127.0.0.1'),
+            'port' => env('OVS_DB_PORT', '3306'),
+            'database' => env('OVS_DB_DATABASE', 'forge'),
+            'username' => env('OVS_DB_USERNAME', 'forge'),
+            'password' => env('OVS_DB_PASSWORD', ''),
+            'unix_socket' => env('OVS_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
