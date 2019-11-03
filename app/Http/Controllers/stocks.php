d1a840907c1de751480c2c179e8460e3e50c93fc
Tom Busby <tmbu225@uky.edu>
2019-11-03 18:12:20 -0500

    Added individual stock investment amount logic.

--- app/Http/Controllers/stocks.php
+++ app/Http/Controllers/stocks.php
@@ -11,19 +11,18 @@ class stocks extends Controller
 {
 	public function submitSelection(Request $request)
 	{
+		$invAmount = $request->input('principal') * ($request->input('investment_percent') / 100);
+
+		$amountPerStock = $invAmount / 5;
+
 	    $startDate = $request->input('start_date');
         $endDate = $request->input('end_date');
 
 	    $dateResults = DB::connection('ovs')->select("SELECT * FROM `ovscalendar` where calType = '2' and date_ BETWEEN ? and ? order by date_ asc", [$startDate, $endDate]);
 
-	    $stockResults = [];
-
 		try
 		{
-		    foreach($dateResults as $date)
-		    {
-                $stockResults[] = \FSSCLE::VolitilityHVIVDifference($request->input('start_date'));
-            }
+			$stockResults = \FSSCLE::VolitilityHVIVDifference($request->input('start_date'));
 
 		    dd($stockResults);
 		}
