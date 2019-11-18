bbe44624e3f8ef7318dfbaf42369c1156bcf5d67
Tom Busby <tmbu225@uky.edu>
2019-11-18 15:25:04 -0500

    Refactored code into individual functions.

--- app/Http/Controllers/stocks.php
+++ app/Http/Controllers/stocks.php
@@ -11,102 +11,26 @@ class stocks extends Controller
 {
 	public function submitSelection(Request $request)
 	{
+		// Setup variables
 		$invAmount = $request->input('principle') * ($request->input('investmentPercent') / 100);
-
 		$amountPerStock = $invAmount / 5;
-
 	    $startDate = $request->input('startDate');
         $endDate = $request->input('endDate');
 
+		// Get the valid trading days
 	    $dateResults = DB::connection('ovs')->select("SELECT * FROM `ovscalendar` where calType = '2' and date_ BETWEEN ? and ? order by date_ asc", [$startDate, $endDate]);
 
-		try
-		{
-			$stockResults = \FSSCLE::VolitilityHVIVDifference($request->input('startDate'));
-		}
-		catch(FSSNotFoundException $e)
-		{
-			echo $e->getMessage();
-		}
-        catch(InvalidDateFormat $e)
-        {
-            echo $e->getMessage();
-        }
+		// Get the scan results for the first day
+		$stockResults = \FSSCLE::VolitilityHVIVDifference($request->input('startDate'));
 
-		$date = $request->input('startDate');
+		// Get the options list for the first day
+		$optionList = $this->getOptionsList($stockResults, $startDate, $amountPerStock);
 
-		$optionsFound = 0;
-		$counter = 0;
+		// Choose the options near the strike price
+		$chosenOptions = $this->optionSelect($optionList);
 
-		while($optionsFound < 5 && $counter < count($stockResults)) 
-		{
-			// You cannot use named bindings multiple times in the same query.
-			// There must be an individual named binding for each spot.
-			if($amountPerStock > $stockResults[$counter]["price"])
-			{
-				$arguments = [
-					'date1' => $date,
-					'date2' => $date,
-					'date3' => $date,
-					'days_to_exp' => 5,
-					'eq_id' => $stockResults[$counter]['id'],
-					'p_c' => 'C',
-				];
-
-				$query = '
-					SELECT 
-					oc.eqId,
-					DATEDIFF(oc.expDate, :date1) AS daysToExp, 
-					DATEDIFF(oc.expDate, oc.startDate) AS optLength,
-					oc.optId,
-					oc.expDate,
-					op.date_,
-					oc.putCall,
-					oc.strike,
-					op.ask AS optAsk,
-					op.bid AS optBid,
-					eqp.ask,
-					eqp.bid,
-					ivl.ivAsk,
-					ivl.ivBid
-					FROM optcontract AS oc
-					INNER JOIN optprice AS op ON op.optId=oc.optId AND op.ask > 0 AND op.bid > 0 AND op.date_=:date2
-					INNER JOIN eqprice AS eqp ON eqp.eqId = oc.eqId AND eqp.date_ = op.date_
-					INNER JOIN ivlisted AS ivl ON ivl.optId = oc.optId AND ivl.date_ = op.date_
-					WHERE DATEDIFF(oc.expDate, :date3) >= :days_to_exp AND oc.eqId=:eq_id AND oc.putCall=:p_c AND oc.strike > eqp.ask
-
-					ORDER by oc.strike ASC, oc.expDate ASC
-				';
-
-				$callResult = DB::connection('ovs')->select($query, $arguments);
-
-				$callResults[] = $callResult;
-				$optionsFound++;
-			}	
-
-			$counter++;
-		}
-
-		$chosenOptions = $this->optionSelect($callResults);
-
-		foreach($chosenOptions as $option)
-		{
-			$arguments = [
-				'optId' => $option->optId,
-				'startDate' => $startDate,
-				'endDate' => $endDate,
-			];
-
-			$query = "
-				SELECT *
-				FROM `optprice`
-				WHERE optId=:optId and date_ BETWEEN :startDate and :endDate
-			";
-
-			$result = DB::connection('ovs')->select($query, $arguments);
-
-			$priceHistory[$option->optId] = $result;
-		}
+		// The the history of the chosen options
+		$priceHistory = $this->getPriceHistory($chosenOptions, $startDate, $endDate);
 
 		dd($priceHistory);
 	}
@@ -155,4 +79,87 @@ class stocks extends Controller
 
         return $theChosenOnes;
     }
+
+	// This takes in a list of options and gets the price history
+	// from a start date to an end date.
+	private function getPriceHistory($chosenOptions, $startDate, $endDate)
+	{
+		foreach($chosenOptions as $option)
+		{
+			$arguments = [
+				'optId' => $option->optId,
+				'startDate' => $startDate,
+				'endDate' => $endDate,
+			];
+
+			$query = "
+				SELECT *
+				FROM `optprice`
+				WHERE optId=:optId and date_ BETWEEN :startDate and :endDate
+			";
+
+			$result = DB::connection('ovs')->select($query, $arguments);
+
+			$priceHistory[$option->optId] = $result;
+		}
+
+		return $priceHistory;
+	}
+
+	private function getOptionsList($stocks, $date, $amountPerStock)
+	{
+		$optionsFound = 0;
+		$counter = 0;
+
+		while($optionsFound < 5 && $counter < count($stocks)) 
+		{
+			// You cannot use named bindings multiple times in the same query.
+			// There must be an individual named binding for each spot.
+			if($amountPerStock > $stocks[$counter]["price"])
+			{
+				$arguments = [
+					'date1' => $date,
+					'date2' => $date,
+					'date3' => $date,
+					'days_to_exp' => 5,
+					'eq_id' => $stocks[$counter]['id'],
+					'p_c' => 'C',
+				];
+
+				$query = '
+					SELECT 
+					oc.eqId,
+					DATEDIFF(oc.expDate, :date1) AS daysToExp, 
+					DATEDIFF(oc.expDate, oc.startDate) AS optLength,
+					oc.optId,
+					oc.expDate,
+					op.date_,
+					oc.putCall,
+					oc.strike,
+					op.ask AS optAsk,
+					op.bid AS optBid,
+					eqp.ask,
+					eqp.bid,
+					ivl.ivAsk,
+					ivl.ivBid
+					FROM optcontract AS oc
+					INNER JOIN optprice AS op ON op.optId=oc.optId AND op.ask > 0 AND op.bid > 0 AND op.date_=:date2
+					INNER JOIN eqprice AS eqp ON eqp.eqId = oc.eqId AND eqp.date_ = op.date_
+					INNER JOIN ivlisted AS ivl ON ivl.optId = oc.optId AND ivl.date_ = op.date_
+					WHERE DATEDIFF(oc.expDate, :date3) >= :days_to_exp AND oc.eqId=:eq_id AND oc.putCall=:p_c AND oc.strike > eqp.ask
+
+					ORDER by oc.strike ASC, oc.expDate ASC
+				';
+
+				$callResult = DB::connection('ovs')->select($query, $arguments);
+
+				$callResults[] = $callResult;
+				$optionsFound++;
+			}	
+
+			$counter++;
+		}
+
+		return $callResults;
+	}
 }
